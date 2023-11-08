865d79cc7fa4e9377c7bc8968f53f8a2a80df6e4
Ville Penttinen <villem.penttinen@gmail.com>
2023-11-08 18:48:05 +0200

    Enable SpanToken(izer) support for all supported frameworks
    Adds dependency to System.Memory for netstandard2.0

--- src/SharpDotEnv/Internal/SpanToken.cs
+++ src/SharpDotEnv/Internal/SpanToken.cs
@@ -1,7 +1,5 @@
-﻿
-namespace SharpDotEnv.Internal
+﻿namespace SharpDotEnv.Internal
 {
-#if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
     using System;
 
     internal readonly ref struct SpanToken
@@ -18,20 +16,6 @@ namespace SharpDotEnv.Internal
 
         public override string ToString() => $"{Type}='{Value.ToString()}'";
     }
-#else
-    // simple implementation to ensure tests compile
-    internal readonly struct SpanToken
-    {
-        public SpanToken(TokenType type, string value)
-        {
-            Type = type;
-            Value = value;
-        }
-        public TokenType Type { get; }
-        public string Value { get; }
-        public override string ToString() => $"{Type}='{Value}'";
-    }
-#endif
 
     internal static class SpanTokenExtensions
     {
