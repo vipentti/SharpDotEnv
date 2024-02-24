0ba74894c1c54dd168eb6d88a75fed69c445fb49
Ville Penttinen <villem.penttinen@gmail.com>
2024-02-24 06:08:04 +0000

    Convert to file-scoped namespaces

--- src/SharpDotEnv/Internal/StreamToken.cs
+++ src/SharpDotEnv/Internal/StreamToken.cs
@@ -2,20 +2,19 @@
 // Distributed under the MIT License.
 // https://github.com/vipentti/SharpDotEnv/blob/main/LICENSE.md
 
-namespace SharpDotEnv.Internal
+namespace SharpDotEnv.Internal;
+
+internal readonly struct StreamToken
 {
-    internal readonly struct StreamToken
+    public StreamToken(TokenType type, string value)
     {
-        public StreamToken(TokenType type, string value)
-        {
-            Type = type;
-            Value = value;
-        }
-
-        public TokenType Type { get; }
-
-        public string Value { get; }
-
-        public override string ToString() => $"{Type}='{Value}'";
+        Type = type;
+        Value = value;
     }
+
+    public TokenType Type { get; }
+
+    public string Value { get; }
+
+    public override string ToString() => $"{Type}='{Value}'";
 }
